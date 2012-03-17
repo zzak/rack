c72121aa0fcaebc5e38365f50ea1f9be52cc3a06
James Tucker <jftucker@gmail.com>
2012-03-17 12:50:59 -0700

    Correct some of the mime type issues. References #316 and #366. HTTP 1.0 and 1.1 do not have MUST for Content-Type requirements, they have "should" (not SHOULD). They also have text describing how clients should handle this header being missing.

--- lib/rack/file.rb
+++ lib/rack/file.rb
@@ -21,9 +21,10 @@ module Rack
 
     alias :to_path :path
 
-    def initialize(root, cache_control = nil)
+    def initialize(root, cache_control = nil, default_mime = 'text/plain')
       @root = root
       @cache_control = cache_control
+      @default_mime = default_mime
     end
 
     def call(env)
@@ -70,15 +71,13 @@ module Rack
     def serving(env)
       last_modified = F.mtime(@path).httpdate
       return [304, {}, []] if env['HTTP_IF_MODIFIED_SINCE'] == last_modified
-      response = [
-        200,
-        {
-          "Last-Modified"  => last_modified,
-          "Content-Type"   => Mime.mime_type(F.extname(@path), 'text/plain')
-        },
-        env["REQUEST_METHOD"] == "HEAD" ? [] : self
-      ]
-      response[1].merge! 'Cache-Control' => @cache_control if @cache_control
+      
+      headers = { "Last-Modified" => last_modified }
+      mime = Mime.mime_type(F.extname(@path), @default_mime)
+      headers["Content-Type"] = mime if mime
+      headers['Cache-Control'] = @cache_control if @cache_control
+
+      response = [ 200, headers, env["REQUEST_METHOD"] == "HEAD" ? [] : self ]
 
       # NOTE:
       #   We check via File::size? whether this file provides size info
